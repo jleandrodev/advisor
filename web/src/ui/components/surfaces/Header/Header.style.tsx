e92333ea945e16777cc041ec2ae560046186bf83
Ozório <jleandro.dev@gmail.com>
2023-06-03 16:53:27 -0300

    feat: Componente Header iniciado

---
+++ web/src/ui/components/surfaces/Header/Header.style.tsx
@@ -0,0 +1,3 @@
+import { styled } from "@mui/material";
+
+export const HeaderContainer = styled("header")``;
---
+++ web/src/ui/components/surfaces/Header/Header.tsx
@@ -0,0 +1,11 @@
+import { HeaderContainer } from "./Header.style";
+
+const Header: React.FC = () => {
+  return (
+    <HeaderContainer>
+      <h1>Header</h1>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
